7fba415c1715643a12c278422c41d7360cb15141
Uriel Sánchez <17171295@itculiacan.edu.mx>
2020-04-22 23:55:03 -0600

    blog area completed

--- js/main.js
+++ js/main.js
@@ -4,6 +4,18 @@ $(document).ready(() => {
 
     $toggleCollapse.click(() => {
         $nav.toggleClass('collapse');
-    })
+    });
+
+
+    //owl carousel
+    $('.owl-carousel').owlCarousel({
+        loop: true,
+        autoplay: true,
+        autoplayTimeout: 3000,
+        dots: false,
+        nav: true,
+        navText: [$('.owl-navigation .owl-nav-prev'), $('.owl-navigation .owl-nav-next')],
+        autoplayHoverPause: true
+    });
 
 });
